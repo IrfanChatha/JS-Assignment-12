8a0ea04eb083df960a3d72e5ce8fa3b890d661d3
Irfan M. Chatha ✅ <118846772+IrfanChatha@users.noreply.github.com>
2023-07-27 23:35:11 +0500

    Assignment Updated

---
+++ script.js
@@ -0,0 +1,196 @@
+// String Handling
+// 1. Write a js program to find length of a string.
+// let abcString = "Hello World SMIT";
+// console.log(abcString.length);
+
+// 2. Write a js program to copy one string to another string.
+// let abcString = "Hello World";
+// let bcdString = abcString;
+// console.log(bcdString);
+
+// 3. Write a js program to concatenate two strings.
+// let abcString = "Hello";
+// let bcdString = "World";
+// console.log(abcString + " " + bcdString);
+// console.log(abcString.concat(" " + bcdString));
+
+// 4. Write a js program to compare two strings.
+// let abcString = "Hello";
+// let bcdString = "World";
+// if (abcString === bcdString) {console.log("Both strings are same");}
+// else console.log("Strings are different");
+
+// 5. Write a js program to convert lowercase string to uppercase.
+// let abcString = "HelLO";
+// console.log(abcString.toUpperCase());
+
+// 6. Write a js program to convert uppercase string to lowercase.
+// let abcString = "HELLO";
+// console.log(abcString.toLowerCase());
+
+// 7. Write a js program to toggle case of each character of a string.
+// let abcString = "hELLo WoRld";
+// let toggString = "";
+// for (let i=0; i < abcString.length; i++){
+// let char = abcString[i];
+// if (char === char.toUpperCase()){
+//   toggString += char.toLowerCase();
+// }
+// else toggString += char.toUpperCase();
+// }
+// console.log(toggString);
+
+// 8. Write a js program to find total number of alphabets, digits or special character in a string.
+// let newString = "Hello @ 7 World 5";
+// let alph = 0;
+// let digi = 0;
+// let spl = 0;
+
+// for (let i = 0; i < newString.length; i++)
+// {
+// if (/[a-zA-Z]/.test(newString[i])) {
+//   alph++;
+// } else if (/[0-9]/.test(newString[i])) {
+//   digi++;
+// } else {
+//   spl++;
+// }}
+// console.log("Number of Alphabets: " +alph, "\nNumber of Digits: "+digi, "\nNumber of Special Characters: "+spl);
+
+// 9. Write a js program to count total number of vowels and consonants in a string.
+// let abcString = "This is a kite";
+// let vowels = 0;
+// let consonant = 0;
+// let spaace = 0;
+
+// for (let i = 0; i < abcString.length; i++)
+// {
+//  if (/[a,e,i,o,u,A,E,I,O,U]/.test(abcString[i])) {
+//    vowels++;
+// } else if (/[ ]/.test(abcString[i])) {
+//    spaace++;
+// }
+// else {
+//    consonant++;
+//  }}
+// console.log("Number of Vowels: " +vowels, "\nNumber of Consonants: " +consonant);
+
+// 10. Write a js program to count total number of words in a string.
+// let abcString = "This is a kite";
+// function WordCount(str) { 
+//     return str.split(" ").length;
+//   }
+// console.log(WordCount(abcString));
+
+// 11. Write a js program to find reverse of a string.
+// let abcString = "This is a kite";
+// let revStr = "";
+// function reverseStr(str)
+// {
+//     for (let i = abcString.length-1; i >= 0 ; i--){
+//     revStr += str[i];
+// }
+//     return revStr;
+// }
+// console.log(reverseStr(abcString));
+
+// 12. Write a js program to check whether a string is palindrome or not.
+
+// 13. Write a js program to reverse order of words in a given string
+
+// 14. Write a js program to find first occurrence of a character in a given string.
+
+// 15. Write a js program to find last occurrence of a character in a given string.
+
+// 16. Write a js program to search all occurrences of a character in given string.
+
+// 17. Write a js program to count occurrences of a character in given string.
+
+// 18. Write a js program to find highest frequency character in a string.
+
+// 19. Write a js program to find lowest frequency character in a string.
+
+// 20. Write a js program to count frequency of each character in a string.
+
+// 21. Write a js program to remove first occurrence of a character from string.
+
+// 22. Write a js program to remove last occurrence of a character from string.
+
+// 23. Write a js program to remove all occurrences of a character from string.
+
+// 24. Write a js program to remove all repeated characters from a given string.
+
+// 25. Write a js program to replace first occurrence of a character with another in a string.
+
+// 26. Write a js program to replace last occurrence of a character with another in a string.
+
+// 27. Write a js program to replace all occurrences of a character with another in a string.
+
+// 28. Write a js program to find first occurrence of a word in a given string.
+
+// 29. Write a js program to find last occurrence of a word in a given string.
+
+// 30. Write a js program to search all occurrences of a word in given string.
+
+// 31. Write a js program to count occurrences of a word in a given string.
+
+// 32. Write a js program to remove first occurrence of a word from string.
+
+// 33. Write a js program to remove last occurrence of a word in given string.
+
+// 34. Write a js program to remove all occurrence of a word in given string.
+
+// 35. Write a js program to trim leading white space characters from given string.
+
+// 36. Write a js program to trim trailing white space characters from given string.
+
+// 37. Write a js program to trim both leading and trailing white space characters from given string.
+
+// 38. Write a js program to remove all extra blank spaces from given string.
+
+// Array Handling
+// 1. Write a js program to read and print elements of array.
+
+// 2. Write a js program to print all negative elements in an array.
+
+// 3. Write a js program to find sum of all array elements. 
+
+// 4. Write a js program to find maximum and minimum element in an array.
+
+// 5. Write a js program to find second largest element in an array.
+
+// 6. Write a js program to count total number of even and odd elements in an array.
+
+// 7. Write a js program to count total number of negative elements in an array.
+
+// 8. Write a js program to copy all elements from an array to another array.
+
+// 9. Write a js program to insert an element in an array.
+
+// 10. Write a js program to delete an element from an array at specified position.
+
+// 11. Write a js program to count frequency of each element in an array.
+
+// 12. Write a js program to print all unique elements in the array.
+
+// 13. Write a js program to count total number of duplicate elements in an array.
+
+// 14. Write a js program to delete all duplicate elements from an array.
+
+// 15. Write a js program to merge two array to third array.
+
+// 16. Write a js program to find reverse of an array.
+
+// 17. Write a js program to put even and odd elements of array in two separate array.
+
+// 18. Write a js program to search an element in an array.
+
+// 19. Write a js program to sort array elements in ascending or descending order.
+
+// 20. Write a js program to sort even and odd elements of array separately.
+
+// 21. Write a js program to left rotate an array.
+
+// 22. Write a js program to right rotate an array.
+
+
